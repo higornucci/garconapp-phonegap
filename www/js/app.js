e594bf275972463733fb240ab41bc0dc759475f1
unknown <higor.nucci@dominio.ufms.br>
2017-09-25 08:53:04 -0400

    camera para capturar o numero da mesa com qrcode

---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+$('.modal-trigger').leanModal();
+
+$('.collection-item').on('click', function(){
+    var $badge = $('.badge', this);
+    if($badge.length == 0) {
+        $badge = $('<span class="badge brown-text">0</span>')
+        .appendTo(this);
+    }
+    $badge.text(parseInt($badge.text()) + 1);
+    var nomeProduto = this.firstChild.textContent;
+    Materialize.toast(nomeProduto + ' adicionado', 1000);
+});
+
+$('#confirmar').on('click', function() {
+    var texto = "";
+
+    $('.badge').parent().each(function(){
+        texto += this.firstChild.textContent + ': ';
+        texto += this.lastChild.textContent + ', ';
+    });
+
+    $('#resumo').empty().text(texto);
+});
+
+$('.collection')
+.on('click', '.badge', function() {
+    $(this).remove();
+    return false;
+});
+
+$('.acao-limpar').on('click', function() {
+    $('#numero-mesa').val('');
+    $('.badge').remove();
+});
+
+$('.scan-qrcode').on('click', function(){
+    cordova.plugins.barcodeScanner.scan(
+       function (resultado) {
+           if (resultado.text) {
+               Materialize.toast('Mesa ' + resultado.text, 2000);
+               $('#numero-mesa').val(resultado.text);
+           }
+       },
+       function (error) {
+           Materialize.toast('Erro: ' + error, 3000, 'red-text');
+       }
+    );
+});
